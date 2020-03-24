a6f27dc8baab53c878ab97f441515fa3c6fc4081
Bill Cathey <billcathey@hotmail.com>
2020-03-24 12:31:23 -0400

    First commit

---
+++ src/build/precache-manifest.4b80aaf7fb43d2922886720887655f9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6b45d0651b390e0b2f2f8ea21044678",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69749eeffb96457c7f11",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "8113c7008084799f8ba3",
+    "url": "/static/js/2.c105c0a6.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.c105c0a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69749eeffb96457c7f11",
+    "url": "/static/js/main.650ff87a.chunk.js"
+  },
+  {
+    "revision": "f8c88a4ab800e2a20047",
+    "url": "/static/js/runtime-main.ae3b9067.js"
+  }
+]);
